714d03572c0d954e68ff4f39d7938e02ef0ae93b
kjdiaz508 <diazkevin508@gmail.com>
2024-09-03 01:08:27 -0700

    did first few

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let c = (5/9) * (temp - 32);
+  return Math.round(c * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let f = (9/5) * c + 32;
+  return Math.round(f * 10) / 10;
 };
 
 // Do not edit below this line
