13aa5bc8c971471cefb49d90efa7ccd6f92d13fe
kjdiaz508 <diazkevin508@gmail.com>
2024-09-06 19:39:13 -0700

    did the rest of the exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    const isAlphaNum = function(char){
+        code = char.charCodeAt(0);
+        return (
+            (code >= 48 && code <= 57) ||
+            (code >= 65 && code <= 90) ||
+            (code >= 97 && code <= 122)
+        );
+    }
+    const forwards = text
+        .toLowerCase()
+        .split('')
+        .filter(char => isAlphaNum(char))
+        .join('')
+    const backwards = forwards.split('').reverse().join('')
 
+    return forwards === backwards;
 };
 
 // Do not edit below this line
